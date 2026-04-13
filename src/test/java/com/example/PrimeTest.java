1ecfe5824150028b41edd7355b457d6b9d0791a1
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-13 12:16:44 +0530

    Okay

--- src/test/java/com/example/PrimeTest.java
+++ src/test/java/com/example/PrimeTest.java
@@ -1,7 +1,7 @@
 package com.example;
 import org.junit.Test;
 import static org.junit.Assert.*;
-public class EvenTest{
+public class PrimeTest{
     Prime e=new Prime();
     @Test
     public void even(){
