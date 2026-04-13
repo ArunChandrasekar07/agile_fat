7dea3bedb0973ba4ab1285e8bfa44468bc525271
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-13 12:17:17 +0530

    Okay

--- src/test/java/com/example/PrimeTest.java
+++ src/test/java/com/example/PrimeTest.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.*;
 public class PrimeTest{
     Prime e=new Prime();
     @Test
-    public void even(){
+    public void prime(){
         assertEquals("Prime",e.check(2));
     }
     @Test
-    public void odd(){
+    public void notprime(){
         assertEquals("Not Prime",e.check(7));
     }
 }
